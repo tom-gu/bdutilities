97b005331419ef26d023bd275e0ed9ca6ece2f08
tom-gu <tomer.gu@gmail.com>
2020-09-01 05:55:14 +0300

    Adding #Hex color of each pkg to bdverse_core_pkgs

--- R/bdverse_core_pkgs.R
+++ R/bdverse_core_pkgs.R
@@ -1,4 +1,8 @@
 #' @title All `bdverse` core packages
-#' @description All the names of `bdverse` **core** packages (eight packages)
-#' @format A character vector with eight elements
+#' @description basic info  on all of `bdverse` **core** packages (eight packages)
+#' @format A data frame with 8 rows and 2 variables:
+#' \describe{
+#'   \item{\code{pkg_name}}{Pakcage name}
+#'   \item{\code{pkg_color}}{Package color (Hex color code)} 
+#'}
 "bdverse_core_pkgs"
--- data-raw/bdverse_core_pkgs.R
+++ data-raw/bdverse_core_pkgs.R
@@ -1,14 +1,25 @@
 ## code to prepare `bdverse_core_pkgs` dataset goes here:
 
-bdverse_core_pkgs <- c(
-                       "bdutilities",
-                       "bdutilities.app",
-                       "bdDwC",
-                       "bddwc.app",
-                       "bdchecks",
-                       "bdchecks.app",
-                       "bdclean",
-                       "bdverse"
-                       )
+bdverse_core_pkgs <- data.frame(pkg_name = c(
+                                            "bdverse",
+                                            "bdDwC",
+                                            "bddwc.app",
+                                            "bdchecks",
+                                            "bdchecks.app",
+                                            "bdclean",
+                                            "bdutilities",
+                                            "bdutilities.app"
+                                            ),
+                                pkg_color = c(
+                                            "#265b6d",
+                                            "#785549",
+                                            "#785549",
+                                            "#4BAE4D",
+                                            "#4BAE4D",
+                                            "#37a5dd",
+                                            "#939598",
+                                            "#939598"
+                                            )
+                                )
 
 usethis::use_data(bdverse_core_pkgs, overwrite = TRUE) # add data to pkg
